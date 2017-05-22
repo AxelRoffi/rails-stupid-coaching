8166d7ff81bc23db5027ddf57034e94296fa1235
Axel Roffi <axel.roffi@gmail.com>
2017-05-22 15:01:52 +0200

    RoR stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'coaching/ask'
-
-  get 'coaching/answer'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get 'ask' => "coaching#ask"
+
+  get 'answer' =>'coaching#answer'
+
+
+
 end
-
-
